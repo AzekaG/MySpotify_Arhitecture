7d09ab38543ddabe683378bc9bdb516297656c08
MatvX_lm73 <MatvX_lm73@student.itstep.org>
2024-02-09 22:54:56 +0200

    Добавьте файлы проекта.

---
+++ MySpotify_Dal_Bll/Controllers/MediaController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySpotify.BLL.DTO;
+using MySpotify.BLL.Interfaces;
+using MySpotify.DAL.Entities;
+using MySpotify.Models;
+
+namespace MySpotify.Controllers
+{
+    public class MediaController : Controller
+    {
+        readonly IUserService _userService;
+        readonly IGenreService _genreService;
+        readonly IMediaService _mediaService;
+        readonly IWebHostEnvironment _environment;
+        public MediaController(IUserService userService, IGenreService genreService, IMediaService mediaService , IWebHostEnvironment environment)
+        {
+            _userService = userService;
+            _genreService = genreService;
+            _mediaService = mediaService;
+            _environment = environment;
+        }
+
+
+        // GET: MediaControlelr
+        public async Task<IActionResult> Index()
+        {
+            if(!isLogged())
+            {
+                return View("IndexUnlogged", new MediaUserGenreModel
+                {
+                    mediaDTOs = await _mediaService.GetMediaList(),
+                    userDTO = null
+                });
+            }
+            var user = await _userService.GetUser(int.Parse(HttpContext.Session.GetString("Id")));
+            return View("IndexLogged", new MediaUserGenreModel
+            {
+                mediaDTOs = await _mediaService.GetMediaList(),
+                userDTO = user,
+                mediaUserDTOs = (await _mediaService.GetMediaList()).Where(x=>x.UserId == user.Id )
+            });
+        }
+
+
+
+
+        // GET: MediaControlelr/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MediaControlelr/Create
+        public async Task<ActionResult> Create()
+        {
+			MediaDownloadModel mediaDownloadModel = new MediaDownloadModel() 
+            {
+                Genres = await _genreService.GetGenres(),
+               
+            };
+			return View(mediaDownloadModel);
+        }
+
+        // POST: MediaControlelr/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(MediaDownloadModel media, IFormFile? upPoster, IFormFile? upMedia , string genre)
+        {
+
+            media.MediaDTO.Genre = (await _genreService.Get(int.Parse(genre))).Name;
+            
+            if (!isLogged())
+            {
+                return RedirectToAction("Index", "Media");
+            }
+
+            string MediaAdress = String.Empty;
+            string PosterAdress = String.Empty;
+
+            if (upPoster != null && upMedia != null)
+            {
+                MediaAdress = "/Media/Music/" + upMedia.FileName;
+                PosterAdress = "/Media/Poster/" + upPoster.FileName;
+            }
+            media.MediaDTO.FileAdress = MediaAdress;
+            media.MediaDTO.Poster = PosterAdress;
+            media.MediaDTO.UserId = int.Parse(HttpContext.Session.GetString("Id"));
+            media.MediaDTO.rootPath = _environment.WebRootPath;
+			 
+            if (ModelState.IsValid)
+            {
+                await _mediaService.CreateMedia(media.MediaDTO , upPoster , upMedia);
+                return RedirectToAction("Index", "Media");
+            }
+            return View(media);
+        }
+
+        // GET: MediaControlelr/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MediaControlelr/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MediaControlelr/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MediaControlelr/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        bool isLogged()
+        {
+            if (HttpContext.Session.GetString("Id") != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ MySpotify_Dal_Bll/Models/LoginModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MySpotify.Models
+{
+    public class LoginModel
+    {
+
+            [Required]
+            [DisplayName("Почтовый ящик")]
+            public string? Email { get; set; }
+            [Required]
+            [DisplayName("Пароль")]
+            [DataType(DataType.Password)]
+            public string? Password { get; set; }
+
+    }
+
+
+
+}
+
